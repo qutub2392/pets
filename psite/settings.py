16e865ff083d79b8863f1f98afa2395db3a83e95
Qutub Rehman <qutub2392@gmail.com>
2018-03-12 00:33:15 +0530

    settings

--- psite/settings.py
+++ psite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sk)2tdwz@z761#fhi3dogng!0ai%#6w$_(o@(r^6_hyemj@e@7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com','localhost','petsline.in','petslineindia.com']
 
 
 # Application definition
